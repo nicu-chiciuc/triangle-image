14c0f6c19e292eff1db2fe32417e78fc7e1d985b
nicu-chiciuc <nicuchiciuc@gmail.com>
2017-09-19 20:59:43 +0300

    Formatted the code with prettier and also changed some small things.

--- script.js
+++ script.js
@@ -1,120 +1,117 @@
-let triangulate = require('delaunay-triangulate')
+let triangulate = require("delaunay-triangulate");
 
-const width = 600
-const height = 400
-
+const width = 600;
+const height = 400;
 
 window.onload = function(event) {
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  canvas.height = height;
+  canvas.width = width;
 
-	let canvas = document.getElementById('canvas')
-	let ctx = canvas.getContext('2d')
-	canvas.height = height
-	canvas.width = width
+  let back = document.getElementById("back");
+  back.height = height;
+  back.width = width;
+  let bctx = back.getContext("2d");
 
-	let back = document.getElementById('back')
-	back.height = height
-	back.width = width
-	let bctx = back.getContext('2d')
+  // let img = document.getElementById('img.jpg')
+  let img = new Image();
+  img.crossOrigin = "anonymous";
+  img.src = "./img.jpg";
 
-	// let img = document.getElementById('img.jpg')
-	let img = new Image()
-	img.crossOrigin="anonymous"
-	img.src = './img.jpg'
+  img.onload = () => {
+    bctx.drawImage(img, 0, 0, width, height);
 
-	img.onload = () => {
+    function drawPoint({ x, y }) {
+      console.log(x, y);
+      ctx.fillRect(x, y, 5, 5);
+    }
 
-		bctx.drawImage(img, 0, 0, width, height)
+    let points = [];
+    for (let i = 0; i < 1500; i++)
+      points.push({
+        x: Math.random() * width,
+        y: Math.random() * height,
+        spdx: Math.random() * 0.4 + 0.1,
+        spdy: Math.random() * 0.4 + 0.1
+      });
 
+    // console.log(pntsArr)
 
-		function drawPoint ({x, y}) {
-			console.log(x, y)
-			ctx.fillRect(x, y, 5, 5)
-		}
+    function getColorAtPoint(context, { x, y }) {
+      return context.getImageData(parseInt(x), parseInt(y), 1, 1).data;
+    }
 
-		let points = []
-		for (let i=0; i<1500; i++)
-			points.push({x: Math.random()*width, y: Math.random()*height,
-				spdx: Math.random()*0.4 + 0.1, spdy: Math.random()*0.4 + 0.1})
+    function get3ColorAvg(context, pnts) {
+      let c = pnts.map(pnt => getColorAtPoint(context, pnt));
+      // console.log(c)
+      let avg = [
+        parseInt((c[0][0] + c[1][0] + c[2][0]) / 3),
+        parseInt((c[0][1] + c[1][1] + c[2][1]) / 3),
+        parseInt((c[0][2] + c[1][2] + c[2][2]) / 3)
+      ];
 
-		// console.log(pntsArr)
+      return avg;
+    }
 
-		function getColorAtPoint (context, {x, y}) {
-			return context.getImageData(parseInt(x), parseInt(y), 1, 1).data
-		}
+    // getColorAtPoint(ctx, {x:200, y:200})
+    // ctx.fillRect(200, 100, 10, 10)
 
-		function get3ColorAvg (context, pnts) {
-			let c = pnts.map(pnt => getColorAtPoint(context, pnt))
-			// console.log(c)
-			let avg = [
-				parseInt((c[0][0] + c[1][0] + c[2][0]) / 3),
-				parseInt((c[0][1] + c[1][1] + c[2][1]) / 3),
-				parseInt((c[0][2] + c[1][2] + c[2][2]) / 3),
-			]
+    function updatePnt(pnt) {
+      pnt.x += pnt.spdx;
+      pnt.y += pnt.spdy;
 
-			return avg
+      pnt.x %= width;
+      pnt.y %= height;
+    }
 
-		}
+    function step() {
+      points.map(updatePnt);
 
-		// getColorAtPoint(ctx, {x:200, y:200})
-		// ctx.fillRect(200, 100, 10, 10)
+      // console.log(points[0])
 
-		function updatePnt (pnt) {
-			pnt.x += pnt.spdx
-			pnt.y += pnt.spdy
+      let pntsArr = points.map(({ x, y }) => [x, y]);
 
-			pnt.x %= width
-			pnt.y %= height
-		}
-		
-		function step () {
-			
+      let triangles = triangulate(pntsArr);
 
-			points.map(updatePnt)
+      triangles.forEach(function(tri, index) {
+        let pntsNow = tri.map(pntNum => points[pntNum]);
 
-			// console.log(points[0])
+        let color = get3ColorAvg(bctx, pntsNow);
 
-			let pntsArr = points.map(({x, y}) => [x, y])
+        tri.color = color;
+      });
 
-			let triangles = triangulate(pntsArr)
+      ctx.globalCompositeOperation = "source-over";
+      ctx.globalAlpha = 0.15;
 
-			triangles.forEach(function(tri, index) {
-				let pntsNow = tri.map(pntNum => points[pntNum])
-				
-				let color = get3ColorAvg(bctx, pntsNow)
+      triangles.forEach(tri => {
+        let pntsNow = tri.map(pntNum => points[pntNum]);
 
-				tri.color = color
+        let color = tri.color;
+        ctx.fillStyle =
+          "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
 
-			});
+        ctx.beginPath();
+        ctx.moveTo(pntsNow[0].x, pntsNow[0].y);
+        ctx.lineTo(pntsNow[1].x, pntsNow[1].y);
+        ctx.lineTo(pntsNow[2].x, pntsNow[2].y);
+        ctx.closePath();
+        ctx.fill();
 
-			ctx.globalCompositeOperation = 'source-over'
-			ctx.globalAlpha = 0.15
+        // ctx.stroke()
+      });
 
-			triangles.forEach(tri => {
-				let pntsNow = tri.map(pntNum => points[pntNum])
+      ctx.globalCompositeOperation = "destination-in";
+      ctx.globalAlpha = 1;
+      ctx.font = "bold 360px hoge,impact";
+      // ctx.fillText("mihaela",50,350);
 
-				let color = tri.color
-				ctx.fillStyle = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')'
+      window.requestAnimationFrame(step);
+    }
 
-				ctx.beginPath()
-				ctx.moveTo(pntsNow[0].x, pntsNow[0].y)
-				ctx.lineTo(pntsNow[1].x, pntsNow[1].y)
-				ctx.lineTo(pntsNow[2].x, pntsNow[2].y)
-				ctx.closePath()
-				ctx.fill()
+    window.requestAnimationFrame(step);
+  };
 
-				// ctx.stroke()
-			})
-
-			ctx.globalCompositeOperation = 'destination-in'
-			ctx.globalAlpha = 1
-			ctx.font = "bold 360px hoge,impact";
-			// ctx.fillText("mihaela",50,350);
-
-			window.requestAnimationFrame(step)
-		}
-
-		window.requestAnimationFrame(step)
-	}
-
-	// points.forEach(drawPoint)
-}
+  // points.forEach(drawPoint)
+};
